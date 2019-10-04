85c232f5266a08312e2428ccb052e390e23ce2ea
Shady Alzayat <slyguard.com@gmail.com>
2019-10-03 18:13:39 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/slyguard.github.io/precache-manifest.5c78023444177b85eb2c80e8c35ef95a.js"
+  "/slyguard.github.io/precache-manifest.9831057fdc0d7d95e64e625d59489ecd.js"
 );
 
 self.addEventListener('message', (event) => {
