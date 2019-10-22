91cc859c83122fb74529d3752732c5f9dba699b5
ShadyAlzayat <slyguard.com@gmail.com>
2019-10-22 10:47:20 -0700

    Updates

--- s-w.js
+++ s-w.js
@@ -1,16 +1,3 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
