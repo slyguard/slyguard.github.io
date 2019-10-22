ea9f608fb6bf572914d3d64aaf394399b3571a73
ShadyAlzayat <slyguard.com@gmail.com>
2019-10-22 11:46:42 -0700

    Updates

--- s-w.js
+++ s-w.js
@@ -18,10 +18,10 @@ self.addEventListener('fetch', function(event) {
         return response;
       } else {
         return fetch(event.request).then(function(res) {
-          // return caches.open('dynamic').then(function(cache) {
-          //   cache.put(event.request.url, res.clone());
-          //   return res;
-          // });
+          return caches.open('dynamic').then(function(cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
         });
       }
     })
