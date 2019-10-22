01b062d4d7fcbdbb434cc80aceeb4c034f2e9c2a
ShadyAlzayat <slyguard.com@gmail.com>
2019-10-22 11:56:17 -0700

    Updates

--- s-w.js
+++ s-w.js
@@ -7,8 +7,26 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+// self.addEventListener('activate', function(event) {
+//   console.log('[Service Worker] Activating Service Worker ....', event);
+// });
+
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheWhitelist = ['static'];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
